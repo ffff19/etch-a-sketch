528fb306e5830f77d7a168f05575a63305fa9346
ffff19 <fprogramming@protonmail.com>
2022-05-25 10:06:17 -0300

    Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid

--- e-a-s.js
+++ e-a-s.js
@@ -1,41 +1,68 @@
-let i = 0, j = 0;
+let squares = 16;
+let squareNumber = document.querySelector('.squareNumber');
 
-while (j < 16) {
-	const container = document.querySelector('#container');
-	const row = document.createElement('div');
-	row.setAttribute('id', 'row');
-	row.className = "row";
-	row.id="row" + String(j);
+function makeSquares(squares) {
+	let i = 0, j = 0;
+	while (j < squares) {
+		const container = document.querySelector('#container');
+		const row = document.createElement('div');
+		row.setAttribute('id', 'row');
+		row.className = "row";
+		row.id="row" + String(j);
 
-	container.appendChild(row);
-	j += 1;
-}
-
-j = 0;
-while (j < 16) {
-	r = "#" + "row" + String(j)
-	const row = document.querySelector(r);
-
-	while (i < 16) {
-		const square = document.createElement('div');
-
-		square.classList.add('square');
-
-		row.appendChild(square);
-
-		/* Hovering effect */
-		square.addEventListener("mouseover", event => {
-			square.style.background = 'pink';
-		});
-
-		square.addEventListener("mouseout", event => {
-			square.style.background = 'red';
-		});
-		/* End hovering effect */
-
-		i += 1;
+		container.appendChild(row);
+		j += 1;
 	}
 
-	i = 0;
-	j += 1;
+	j = 0;
+	while (j < squares) {
+		r = "#" + "row" + String(j)
+		const row = document.querySelector(r);
+
+		while (i < squares) {
+			const square = document.createElement('div');
+
+			square.classList.add('square');
+
+			row.appendChild(square);
+
+			/* Hovering effect */
+			square.addEventListener("mouseover", event => {
+				square.style.background = 'brown';
+			});
+
+			square.addEventListener("mouseout", event => {
+				square.style.background = 'pink';
+			});
+			/* End hovering effect */
+
+			i += 1;
+		}
+
+		i = 0;
+		j += 1;
+	}
 }
+
+makeSquares(squares);
+
+squareNumber.addEventListener('click', e => {
+	j = 0;
+	while(j < squares) {
+		r = "#" + "row" + String(j)
+		const row = document.querySelector(r);
+		let i = 0;
+		while (row.children[i]) {
+			let firstNode = row.children[0];
+			row.removeChild(firstNode)
+		}
+		j += 1;
+	}
+	let n = prompt("Please enter the number of squares");
+	if (n <= 100) {
+		squares = n;
+		makeSquares(squares);
+	} else {
+		prompt("Please enter a number lower than 101");
+	}
+});
